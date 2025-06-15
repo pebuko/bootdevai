98f71120ea30ee8b1b4224557ae4fc3f36ff5bb2
pebuko <peterbusfy@seznam.cz>
2025-06-15 20:54:21 +0200

    xxx

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+    print(api_key)
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
